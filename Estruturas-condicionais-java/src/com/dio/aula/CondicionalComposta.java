fed4d1d5099e3916e3142f5495c4bf36eacf4a5d
Joaovr5 <jjoaovitorr.rrocha@gmail.com>
2022-10-06 20:10:35 -0300

    Estrutura condicional composta

---
+++ Estruturas-condicionais-java/src/com/dio/aula/CondicionalComposta.java
@@ -0,0 +1,19 @@
+package com.dio.aula;
+
+public class CondicionalComposta {
+
+	public static void main(String[] args) {
+		
+		//Resultado Escolar 
+		
+		int nota = 6;
+		
+		if(nota >= 7)
+			System.out.println("Aprovado");
+		
+		else
+			System.out.println("Reprovado");
+
+	}
+
+}
