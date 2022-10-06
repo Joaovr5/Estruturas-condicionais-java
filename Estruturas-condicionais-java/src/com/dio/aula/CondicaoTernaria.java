98fff8db80990a99a738f191521b0a3dc869d392
Joaovr5 <jjoaovitorr.rrocha@gmail.com>
2022-10-06 20:30:09 -0300

    Estrutura condicional ternaria

---
+++ Estruturas-condicionais-java/src/com/dio/aula/CondicaoTernaria.java
@@ -0,0 +1,16 @@
+package com.dio.aula;
+
+public class CondicaoTernaria {
+
+	public static void main(String[] args) {
+		
+		//Código simples e mais limpo 
+		
+		int nota = 7;
+		
+		String resultado = nota >= 7 ? "Aprovado" : "Reprovado";
+
+		System.out.println(resultado);
+	}
+
+}
